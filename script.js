98681f7370a668202a0a3accc4a0faee3d70ed0a
Alex <baraban777771962@gmail.com>
2024-05-03 12:06:56 +0100

    second task

--- script.js
+++ script.js
@@ -7,10 +7,18 @@ genres: [],
 private: false
 };
 
-// const a = prompt('Один из просмотренних фильмов?', ''),
-   const  b = prompt('На сколько оцените его?'), 
-    c = prompt('Один из просмотренних фильмов?', ''),
-    d = prompt('На сколько оцените его?');
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++){
+    const  a = prompt('Один из последних просмотренних фильмов?', ''), 
+    b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }
+    else{
+        console.log('error');
+        i = i - 1;
+    }
+}
+
 console.log(personalMovieDB);
